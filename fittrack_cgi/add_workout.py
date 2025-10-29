0e37dd672f9139cf4d6b71e8baf0a0ec2c7ce8cf
Aleksandr Zinovev <azinovev@constructor.university>
2025-10-29 20:10:57 +0100

    Security: Remove real database credentials from all CGI scripts

--- fittrack_cgi/add_workout.py
+++ fittrack_cgi/add_workout.py
@@ -15,9 +15,9 @@ cgitb.enable()
 # Database configuration
 DB_CONFIG = {
     'host': 'localhost',
-    'user': 'azinovev',
-    'password': 'WrtqlLcpgCPs0KGY',
-    'database': 'db_azinovev',
+    'user': 'your_username',
+    'password': '****',
+    'database': 'db_your_username',
     'charset': 'utf8mb4'
 }
 
